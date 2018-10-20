3d96d9970796160215a6027a8190ea917f32c9a8
Tom Victor <vjtomvictor@icloud.com>
2018-10-21 01:11:43 +0530

    conditions

---
+++ src/variables/if condition.kt
@@ -0,0 +1,12 @@
+package variables
+
+fun main(args: Array<String>) {
+    var con:Boolean
+    con = true
+
+    if (con){
+        println("condition is true")
+    }else{
+        println("condition is false")
+    }
+}
---
+++ src/variables/variable-basics.kt
@@ -0,0 +1,19 @@
+package variables
+
+
+fun main(args: Array<String>) {
+    var name: String = "hello world"
+    println(name)
+
+    //val --> constants cant be modified
+    val inte:Int = 12
+    println("integer $inte")
+
+    var pi:Float = 3.14F
+    println("value of pi is $pi")
+
+    var sum:Double = 0.0
+
+    sum = 13.0 + 26.0
+    println("sum : $sum")
+}
